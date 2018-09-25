2b1a895927d1a0ed069fccce89f14b46c3673f71
Xiaoping-Li <narci2008@gmail.com>
2018-09-25 10:34:38 -0700

    Add examples

--- Collections/SymmetricDiff.js
+++ Collections/SymmetricDiff.js
@@ -5,3 +5,7 @@
 // set of elements which are in either of the two sets, but not in both (A △ B = C = {1, 4}). For every additional symmetric difference 
 // you take (say on a set D = {2, 3}), you should get the set with elements which are in either of the two the sets but not both 
 // (C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}). The resulting array must contain only unique values (no duplicates).
+
+// Examples:
+// sym([1, 2, 3], [5, 2, 1, 4, 5]) should return [3, 4, 5].
+// sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) should return [1, 4, 5]
