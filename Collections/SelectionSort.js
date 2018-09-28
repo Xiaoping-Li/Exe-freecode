0facfecd7c85509c8d77fe7b7f80e248b8259f48
Xiaoping-Li <narci2008@gmail.com>
2018-09-28 14:27:20 -0700

    update

--- Collections/SelectionSort.js
+++ Collections/SelectionSort.js
@@ -2,3 +2,6 @@
 // It then starts at the second position, selects the smallest value in the remaining list, and swaps it with the second element. 
 // It continues iterating through the list and swapping elements until it reaches the end of the list. Now the list is sorted. 
 // Selection sort has quadratic time complexity in all cases.
+
+// Instructions: Write a function selectionSort which takes an array of integers as input and returns an array of these integers in 
+// sorted order from least to greatest.
