ca55e45042206a728ffa1520c01ae2f9f2ef1401
Xiaoping-Li <narci2008@gmail.com>
2018-09-28 11:53:50 -0700

    example

--- Collections/Pairwise.js
+++ Collections/Pairwise.js
@@ -6,3 +6,6 @@
 
 // For example pairwise([7, 9, 11, 13, 15], 20) returns 6. The pairs that sum to 20 are [7, 13] and [9, 11]. 
 // We can then write out the array with their indices and values.
+
+// Example:
+// pairwise([0, 0, 0, 0, 1, 1], 1) should return 10.
