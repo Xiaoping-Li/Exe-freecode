04391e08f64bfe1665f8e017dc61400c45f0db40
Xiaoping-Li <narci2008@gmail.com>
2018-09-28 13:03:18 -0700

    solution

--- Collections/BubbleSort.js
+++ Collections/BubbleSort.js
@@ -9,3 +9,24 @@
 // order (the latter one is smaller than the former one). After each iteration, one less element (the last one) is needed to be compared 
 // until there are no more elements left to be compared.
 // https://en.wikipedia.org/wiki/Bubble_sort#/media/File:Bubble-sort-example-300px.gif
+
+// Solutions:
+function bubbleSort(array) {
+  let count = array.length - 1;
+  while (count > 0) {
+    for (let i = 0; i < count; i++) {
+      let current = array[i];
+      let next = array[i + 1];
+      if (current > next) {
+        array[i] = next;
+        array[i + 1] = current;
+      }
+    }
+    count--;
+  }
+  return array;
+}
+
+const array = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
+// const array = [8,7, 5,4,1,6,2,3];
+bubbleSort(array);
