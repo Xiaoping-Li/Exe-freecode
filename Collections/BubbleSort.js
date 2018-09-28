e0401ae74ad6f5344349eb778a38e350f1be2ab7
Xiaoping-Li <narci2008@gmail.com>
2018-09-28 12:33:42 -0700

    create bubblesort

---
+++ Collections/BubbleSort.js
@@ -0,0 +1,3 @@
+// The bubble sort method starts at the beginning of an unsorted array and 'bubbles up' unsorted values towards the end, 
+// iterating through the array until it is completely sorted. It does this by comparing adjacent items and swapping them if they are out 
+// of order. The method continues looping through the array until no swaps occur at which point the array is sorted.
