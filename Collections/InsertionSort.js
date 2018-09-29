ac027275236b4995eaa7340ed58221259dd3b186
Xiaoping-Li <narci2008@gmail.com>
2018-09-29 09:39:17 -0700

    solution

--- Collections/InsertionSort.js
+++ Collections/InsertionSort.js
@@ -10,3 +10,21 @@
 // sort removes one element from the input data, finds the location it belongs within the sorted list, and inserts it there. 
 // It repeats until no input elements remain.
 // https://en.wikipedia.org/wiki/Insertion_sort
+
+// Solutions: Time complexity: Big O(n * n * n) worst case for array.splice Big O (n)
+function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < array[i - 1]) {
+      let temp = array.splice(i, 1)[0];
+      for (let j = i - 1; j >= 0; j--) {
+        if (array[j] > temp && (!array[j - 1] || array[j - 1] <= temp)) {
+          array.splice(j, 0, temp);
+        }
+      }
+    }
+  }
+  return array;
+}
+
+// test array:
+// [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]
