49b7d0140db161d231f755da6a866048c3d95fb1
Xiaoping-Li <narci2008@gmail.com>
2018-09-29 10:11:28 -0700

    update

--- Collections/QuickSort.js
+++ Collections/QuickSort.js
@@ -2,3 +2,6 @@
 // original array. The array is then partitioned into two subarrays of values less than and greater than the pivot value. 
 // We then combine the result of recursively calling the quick sort algorithm on both sub-arrays. This continues until the base case of an 
 // empty or single-item array is reached, which we return. The unwinding of the recursive calls return us the sorted array.
+
+// Quick sort is a very efficient sorting method, providing O(nlog(n)) performance on average. It is also relatively easy to implement. 
+// These attributes make it a popular and useful sorting method.
