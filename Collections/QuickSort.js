923744fed0b642cb5847571faa79521e234ce556
Xiaoping-Li <narci2008@gmail.com>
2018-09-29 10:10:53 -0700

    Quick Sort

---
+++ Collections/QuickSort.js
@@ -0,0 +1,4 @@
+// Quick sort is an efficient, recursive divide-and-conquer approach to sorting an array. In this method, a pivot value is chosen in the 
+// original array. The array is then partitioned into two subarrays of values less than and greater than the pivot value. 
+// We then combine the result of recursively calling the quick sort algorithm on both sub-arrays. This continues until the base case of an 
+// empty or single-item array is reached, which we return. The unwinding of the recursive calls return us the sorted array.
