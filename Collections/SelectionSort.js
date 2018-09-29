6348c928bf58d11a782ad324ba8426dc27263466
Xiaoping-Li <narci2008@gmail.com>
2018-09-28 17:03:44 -0700

    update solution

--- Collections/SelectionSort.js
+++ Collections/SelectionSort.js
@@ -5,3 +5,32 @@
 
 // Instructions: Write a function selectionSort which takes an array of integers as input and returns an array of these integers in 
 // sorted order from least to greatest.
+
+// Solution:
+function selectionSort(array) {
+  // change code below this line
+  const num = array.length - 1;
+  const minObj = {min: array[0], idx: 0};
+  for (let i = 0; i < num; i++) {
+    minObj.min = array[i];
+    minObj.idx = i;
+    for (let j = i + 1; j <= num; j++) {
+      if (array[j] < minObj.min) {
+        minObj.min = array[j];
+        minObj.idx = j;
+      }
+    }
+
+    if (minObj.idx !== i) {
+      let current = array[i];
+      array[i] = minObj.min;
+      array[minObj.idx] = current;
+    }
+  }
+  // change code above this line
+  return array;
+}
+
+const array = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
+// const array = [8,7, 5,4,1,6,2,3];
+selectionSort(array);
