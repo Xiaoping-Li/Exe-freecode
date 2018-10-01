afa188778b82505c626498e1972de363219d5420
Xiaoping-Li <narci2008@gmail.com>
2018-10-01 08:07:47 -0700

    update

--- Collections/CircularQueue.js
+++ Collections/CircularQueue.js
@@ -20,3 +20,13 @@
 // ^Read @ 3
 // ^Write @ 1
 // This approach requires a constant amount of memory but allows files of a much larger size to be processed.
+
+// Instructions:
+// In this challenge we will implement a circular queue. The circular queue should provide enqueue and dequeue methods which allow you to 
+// read from and write to the queue. The class itself should also accept an integer which you can use to specify the size of the queue 
+// when you create it. When you enqueue items to the queue, the write pointer should advance forward and loop back to the beginning once 
+// it reaches the end of the queue. Likewise, the read pointer should advance forward as you dequeue items. The write pointer should not be
+// allowed to move past the read pointer (our class won't let you overwrite data you haven't read yet) and the read pointer should not be 
+// able to advance past data you have written.
+// In addition, the enqueue method should return the item you enqueued if it is successfully and otherwise return null. Similarly, when 
+// you dequeue an item it should be returned and if you cannot dequeue you should return null.
