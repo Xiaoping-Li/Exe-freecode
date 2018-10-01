0b10dc75e760c9e5bf6ec254a8b24e5f00392722
Xiaoping-Li <narci2008@gmail.com>
2018-10-01 07:36:12 -0700

    update

--- Collections/PriorityQueue.js
+++ Collections/PriorityQueue.js
@@ -2,3 +2,10 @@
 // This could be simply represented with an integer. Item priority will override placement order in determining the sequence items are 
 // dequeued. If an item with a higher priority is enqueued after items with lower priority, the higher priority item will be dequeued 
 // before all the others.
+
+// For instance, let’s imagine we have a priority queue with three items:
+// [[’kitten’, 2], [‘dog’, 2], [‘rabbit’, 2]]
+
+// Here the second value (an integer) represents item priority. If we enqueue [‘human’, 1] with a priority of 1 (assuming lower priorities
+// are given precedence) it would then be the first item to be dequeued. The collection would like this:
+// [[‘human’, 1], [’kitten’, 2], [‘dog’, 2], [‘rabbit’, 2]].
