15ee49d47e4ac9fec446c291b78f7cdabc757da1
Xiaoping-Li <narci2008@gmail.com>
2018-10-01 07:35:08 -0700

    Priority Queue

---
+++ Collections/PriorityQueue.js
@@ -0,0 +1,4 @@
+// A Priority Queue is a special type of Queue in which items may have additional information which specifies their priority. 
+// This could be simply represented with an integer. Item priority will override placement order in determining the sequence items are 
+// dequeued. If an item with a higher priority is enqueued after items with lower priority, the higher priority item will be dequeued 
+// before all the others.
