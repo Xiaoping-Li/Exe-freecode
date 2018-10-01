935fe8ca75697bc6a5bc0c7953ee47f5e6ff98bf
Xiaoping-Li <narci2008@gmail.com>
2018-10-01 07:58:20 -0700

    Circular Queue

---
+++ Collections/CircularQueue.js
@@ -0,0 +1,3 @@
+ // A circular queue is basically a queue that writes to the end of a collection then begins over writing itself at the beginning of the 
+ // collection. This is type of data structure has some useful applications in certain situations. For example, a circular queue can be 
+ // used for streaming media. Once the queue is full, new media data simply begins to overwrite old data.
