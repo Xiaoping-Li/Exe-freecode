8a912b2616035a6e50e8351876b23d40ad20c831
Xiaoping-Li <narci2008@gmail.com>
2018-10-02 11:54:54 -0700

    Update SetClass.js

--- Collections/Data Structure/Sets/SetClass.js
+++ Collections/Data Structure/Sets/SetClass.js
@@ -1,2 +1,23 @@
 // A Set is like an array, but it cannot contain duplicate values. The typical use for a Set is to simply check for the presence of an 
 // item. This can be implemented with an object,
+
+function Set() {
+    // the var collection will hold the set
+    var collection = [];
+    // this method will check for the presence of an element and return true or false
+    this.has = function(element) {
+        return (collection.indexOf(element) !== -1);
+    };
+    // this method will return all the values in the set
+    this.values = function() {
+        return collection;
+    };
+    // this method will add an element to the set
+    this.add = function(element) {
+        if(!this.has(element)){
+            collection.push(element);
+            return true;
+        }
+        return false;
+    };
+}
