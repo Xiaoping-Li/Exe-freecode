614bd79c0f6320e160984da0ea52dc07f14a5eca
Xiaoping-Li <narci2008@gmail.com>
2018-10-02 11:12:32 -0700

    Create a Set Class

---
+++ Collections/Set/SetClass.js
@@ -0,0 +1,2 @@
+// A Set is like an array, but it cannot contain duplicate values. The typical use for a Set is to simply check for the presence of an 
+// item. This can be implemented with an object,
