db40f66c6eeb02594e923f610a95d041726fabe1
Xiaoping-Li <narci2008@gmail.com>
2018-10-03 12:37:15 -0700

    Update SetsInES6.js

--- Collections/Data Structure/Sets/SetsInES6.js
+++ Collections/Data Structure/Sets/SetsInES6.js
@@ -1,14 +1,24 @@
 // ES6 contains a built-in data structure Set
 // To create a new empty set:
+
 // var set = new Set();
   // var set = new Set(1);
   // var set = new Set([1, 2, 3]);
+
 // Once you have created a set, you can add the values you wish using the add method:
   // var set = new Set([1, 2, 3]);
   // set.add([4, 5, 6]);
+
 // remove an item from your Set with the delete method.
   // set.delete(1);
+
 // The .has method will check if the value is contained within the set.
   // var hasTwo = set.has(2);
+
 // The .size method will return an integer representing the size of the Set
   // var howBig = set.size;
+
+// spread operator (...) :can take iterable objects in ES6 and turn them into arrays.
+  // var set = new Set([1,2,3]);
+  // var setToArr = [...set]
+  // console.log(setToArr) // returns [ 1, 2, 3 ]
