a1391a632d2b2d6dfc38337e49ccae02d2f38465
Xiaoping-Li <narci2008@gmail.com>
2018-10-03 12:32:14 -0700

    Update SetsInES6.js

--- Collections/Data Structure/Sets/SetsInES6.js
+++ Collections/Data Structure/Sets/SetsInES6.js
@@ -8,3 +8,7 @@
   // set.add([4, 5, 6]);
 // remove an item from your Set with the delete method.
   // set.delete(1);
+// The .has method will check if the value is contained within the set.
+  // var hasTwo = set.has(2);
+// The .size method will return an integer representing the size of the Set
+  // var howBig = set.size;
