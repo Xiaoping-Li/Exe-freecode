e5ab2ed3d1aa07e481f695285a4dda26032ec001
Xiaoping-Li <narci2008@gmail.com>
2018-10-03 12:08:56 -0700

    Update SetsInES6.js

--- Collections/Data Structure/Sets/SetsInES6.js
+++ Collections/Data Structure/Sets/SetsInES6.js
@@ -1,1 +1,8 @@
 // ES6 contains a built-in data structure Set
+// To create a new empty set:
+// var set = new Set();
+  // var set = new Set(1);
+  // var set = new Set([1, 2, 3]);
+// Once you have created a set, you can add the values you wish using the add method:
+  // var set = new Set([1, 2, 3]);
+  // set.add([4, 5, 6]);
