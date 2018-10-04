deb5986d3f23ba6ee2c8beccb33379f871972d1f
Xiaoping-Li <narci2008@gmail.com>
2018-10-04 07:57:35 -0700

    Update Map.js

--- Collections/Data Structure/Hash Table/Map.js
+++ Collections/Data Structure/Hash Table/Map.js
@@ -1,2 +1,20 @@
 // Maps are data structures that store key-value pairs. In JavaScript, these are available to us as objects. Maps provide rapid lookup of 
 // stored items based on key values and are very common and useful data structures.
+
+// The new version of JavaScript provides us with a built-in Map object. This Map object, although similar to regular JavaScript objects, 
+// provides some useful functionality that normal objects lack. For example, an ES6 Map tracks the insertion order of items that are added 
+// to it. Here is a more complete overview of its methods:
+
+// .has(key) returns true or false based on the presence of a key
+
+// .get(key) returns the value associated with a key
+
+// .set(key, value) sets a new key, value pair
+
+// .delete(key) removes a key, value pair
+
+// .clear() removes all key, value pairs
+
+// .entries() returns an array of all the keys in insertion order
+
+// .values() returns an array of all the values in insertion order
