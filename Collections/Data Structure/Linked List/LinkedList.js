9fdcf67a48977a6d9cadaf21c9971fc7cc279906
Xiaoping-Li <narci2008@gmail.com>
2018-10-04 08:46:02 -0700

    Update LinkedList.js

--- Collections/Data Structure/Linked List/LinkedList.js
+++ Collections/Data Structure/Linked List/LinkedList.js
@@ -1,1 +1,2 @@
-LinkedList.js
+// A linked list is a linear collection of data elements, called 'nodes', each of which points to the next. Each node in a linked list 
+// contains two key pieces of information: the element itself, and a reference to the next node.
