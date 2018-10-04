3ea53175ab2f2f8d9828e6f3584d9ae49c3065f6
Xiaoping-Li <narci2008@gmail.com>
2018-10-04 07:43:07 -0700

    Update Map.js

--- Collections/Data Structure/Hash Table/Map.js
+++ Collections/Data Structure/Hash Table/Map.js
@@ -1,1 +1,2 @@
-Map.js
+// Maps are data structures that store key-value pairs. In JavaScript, these are available to us as objects. Maps provide rapid lookup of 
+// stored items based on key values and are very common and useful data structures.
