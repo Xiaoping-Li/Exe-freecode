741ecc3ffba2c5c20f8f95d24970030bae63e9e6
Xiaoping-Li <narci2008@gmail.com>
2018-10-07 08:19:30 -0700

    Update BinarySearchTree.js

--- Collections/Data Structure/Binary Search Tree/BinarySearchTree.js
+++ Collections/Data Structure/Binary Search Tree/BinarySearchTree.js
@@ -1,1 +1,33 @@
-BinarySearchTree.js
+function Node(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+function BinarySearchTree() {
+  this.root = null;
+  // Add node method
+  this.add = function(value) {
+    if (!this.root) {
+      this.root = new Node(value);
+    } else {
+      if (this.root.value >= value) {
+        if (this.root.left) {
+          this.root.left.add(value);
+        } else {
+          this.root.left = new BinarySearchTree();
+          this.root.left.root = new Node(value);
+        }
+      } else {
+        if (this.root.right) {
+          this.root.right.add(value);
+        } else {
+          this.root.right = new BinarySearchTree();
+          this.root.right.root = new Node(value);
+        }
+      }
+    }
+    return value;
+  }
+  // 
+}
